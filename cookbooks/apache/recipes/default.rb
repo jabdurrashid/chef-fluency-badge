00829393ee6f1692d7207bbde3036f0ca612f5f1
Jibril <jibrila@hotmail.com>
2017-01-25 22:18:32 +0000

    added motd and changed apache defualt for node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,6 +5,13 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 #Install apache package
+#
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platfore_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :install
@@ -14,3 +21,5 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
+
